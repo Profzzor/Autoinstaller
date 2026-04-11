a34a3c369ee53a35fe1dc456eba09e4b73aacb9d
Profzzor <146303427+Profzzor@users.noreply.github.com>
2026-04-11 19:49:40 +0530

    Add qt5ct and qt6ct to install script
    Updated installation script to include qt5ct and qt6ct packages.

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y libgl-dev xutils-dev mesa-common-dev libgbm-dev libxcb-s
 sudo apt-get install -y cmake libxau-dev libxext-dev libxinerama-dev libxv-dev libxrender-dev libxdmcp-dev libxcb1-dev libxshmfence-dev libpixman-1-dev libbsd-dev xbitmaps 
 sudo apt-get install -y xkb-data libxfont-dev x11-xkb-utils mesa-utils libgl1-mesa-dri libgbm-dev open-vm-tools open-vm-tools-desktop fonts-font-awesome 
 sudo apt-get install -y git i3-wm i3blocks xinit xterm x11-xserver-utils vim alacritty libkrb5-dev python3-dev thunar python3-pip feh imagemagick picom rofi
-sudo apt-get install -y lxappearance arc-theme adwaita-icon-theme papirus-icon-theme
+sudo apt-get install -y lxappearance qt5ct qt6ct arc-theme adwaita-icon-theme papirus-icon-theme
 
 sudo apt-get remove -y vim-tiny && sudo ln -sf /usr/bin/vim /usr/bin/vi
 
@@ -64,7 +64,10 @@ if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
 fi
 EOF
 
-echo "exec /usr/bin/i3" > ~/.xinitrc
+cat > ~/.xinitrc <<'EOF'
+export QT_QPA_PLATFORMTHEME=qt5ct
+exec /usr/bin/i3
+EOF
 
 cd /tmp && git clone --depth 1 https://github.com/vivien/i3blocks-contrib.git
 
