be0d44e80ce42043ad11e841bf14ec38ca2140d8
Profzzor <146303427+Profzzor@users.noreply.github.com>
2026-04-11 13:33:06 +0530

    Create installation script for X server and i3 setup
    This script installs necessary packages, clones and builds X server and input drivers, configures i3 window manager, and sets up user configurations.

---
+++ install.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y meson ninja-build pkg-config x11proto-dev xorg-dev x11proto-randr-dev xtrans-dev libpixman-1-dev libxkbcommon-x11-dev libxfont-dev libxcvt-dev 
+sudo apt install -y libdrm-dev libepoxy-dev x11proto-present-dev libxkbfile-dev libudev-dev libxshmfence-dev libbsd-dev x11proto-xf86dri-dev libgl1-mesa-dev libglu1-mesa-dev
+sudo apt install -y libgl-dev xutils-dev mesa-common-dev libgbm-dev libxcb-shape0-dev libxcb-util-dev libxcb-icccm4-dev autoconf automake libtool libinput-dev libx11-dev 
+sudo apt install -y cmake libxau-dev libxext-dev libxinerama-dev libxv-dev libxrender-dev libxdmcp-dev libxcb1-dev libxshmfence-dev libpixman-1-dev libbsd-dev xbitmaps 
+sudo apt install -y xkb-data libxfont-dev x11-xkb-utils mesa-utils libgl1-mesa-dri libgbm-dev open-vm-tools open-vm-tools-desktop fonts-font-awesome
+sudo apt install -y git i3-wm i3blocks xinit xterm x11-xserver-utils vim alacritty libkrb5-dev python3-dev thunar python3-pip feh imagemagick picom rofi
+
+sudo apt remove -y vim-tiny && sudo ln -sf /usr/bin/vim /usr/bin/vi
+
+cd /tmp && git clone --depth 1 https://github.com/X11Libre/xserver.git && cd xserver
+
+meson setup build --prefix=/usr/local --localstatedir=/var --sysconfdir=/etc/X11 --buildtype=release -Dxnest=false -Dxvfb=false
+
+ninja -C build
+
+sudo ninja -C build install
+
+export PATH="/usr/local/bin:$PATH"
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/local/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export ACLOCAL_PATH="/usr/share/aclocal:/usr/local/share/aclocal"
+
+cd /tmp && git clone --depth 1 https://github.com/X11Libre/xf86-input-libinput.git && cd xf86-input-libinput
+
+meson setup build --prefix=/usr/local
+
+ninja -C build
+
+sudo ninja -C build install
+
+cd /tmp && git clone --depth 1 https://github.com/X11Libre/xf86-input-keyboard.git && cd xf86-input-keyboard
+
+./autogen.sh --prefix=/usr/local PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/local/lib/x86_64-linux-gnu/pkgconfig"
+
+make && sudo make install
+
+cd /tmp && git clone --depth 1 https://github.com/X11Libre/xf86-input-vmmouse.git && cd xf86-input-vmmouse
+
+./autogen.sh --prefix=/usr/local PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/local/lib/x86_64-linux-gnu/pkgconfig"
+
+make && sudo make install
+
+sudo mv /usr/bin/Xorg /usr/bin/Xorg.xorg-backup
+
+sudo mv /usr/bin/X /usr/bin/X.xorg-backup
+
+sudo apt remove -y xserver-xorg-core
+
+sudo ln -sf /usr/local/bin/Xorg /usr/bin/Xorg
+
+sudo ln -sf /usr/local/bin/Xorg /usr/bin/X
+
+sudo chmod u+s /usr/local/bin/Xorg
+
+cat > ~/.bash_profile <<'EOF'
+if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+  exec startx
+fi
+EOF
+
+echo "exec /usr/bin/i3" > ~/.xinitrc
+
+cd /tmp && git clone --depth 1 https://github.com/vivien/i3blocks-contrib.git
+
+cd i3blocks-contrib/ && sudo make install PREFIX=/usr/local
+
+cd $(find / -type d -name Autoinstaller 2>/dev/null && echo -n 1 )
+
+mkdir -p ~/.config/picom
+cp config/picom.conf ~/.config/picom/
+
+mkdir -p ~/.config/alacritty/
+cp config/alacritty.toml ~/.config/alacritty/
+
+pip install pywal --bre
+
+mkdir -p ~/.config/wal
+cat > ~/.config/wal/config << 'EOF' 
+[backend]
+backend = 'feh'
+options = '--bg-fill'
+EOF
+
+mkdir -p ~/Pictures/
+cp overgrown-green-staircase-forest.jpg ~/Pictures/
+
+XDG_SESSION_TYPE=x11 wal -i ~/Pictures/overgrown-green-staircase-forest.jpg
+
+mkdir -p ~/.config/i3
+cp config/config ~/.config/i3/
+cp config/i3blocks.conf ~/.config/i3/
+cp config/bashrc ~/.bashrc
+
+mkdir -p ~/.config/rofi
+cp config/config.rasi ~/.config/rofi/
+
+fc-cache -fv
+
+# Clean up
+cd /tmp && rm -rf xf86-input-keyboard/ xf86-input-libinput/ xf86-input-vmmouse/ xserver/ i3blocks-contrib/
+
+sudo apt autoremove -y
+
+sudo reboot
